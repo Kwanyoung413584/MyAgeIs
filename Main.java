2bac6b1299c577fc6e8c7c5b778d8618d6b0cc27
Kwanyoung413584 <kwanyoung.kim@patelhs.org>
2020-09-23 17:57:43 +0000

    Added if statement

--- Main.java
+++ Main.java
@@ -2,7 +2,14 @@ class Main {
   public static void main(String[] args) {
     int myAge = 14;
     int myBirthday = 831;
-    int todaysDate = 923;
+    int todaysDate = 921;
     
+    if (myBirthday == todaysDate)
+      System.out.print("I am " + myAge + " years old.");
+    else
+    System.out.print("I am still " + myAge + " years old.");
+
+
+
   }
 }
